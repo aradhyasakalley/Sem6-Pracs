bcde85e7c81d4fa63183589ddd4a098d399b4cc4
aradhyasakalley <sakalleyaradhya@gmail.com>
2024-05-06 19:56:44 +0530

    Aggregate method

---
+++ AA/aggregate-method.py
@@ -0,0 +1,37 @@
+# Multipop stack amortized analysis using aggregate method 
+# Assuming the multipop operation where each multipop can remove at max n elements from the stack and every there can be n such oprations the worst case time complexity turns out to be O(n^2) which is not a very tough bound or and overestimate.
+# Aggregate method 
+# Since for every multipop(k) we need atleast k elements in the stack, hence we can multipop n*n times and the worst case can not be more that O(n) and T(n) making the amortized cost to be T(n)/n 
+
+def push(stack,n,cost):
+    for i in range(n):
+        stack.append(i)
+        cost += 1
+    return stack,cost
+    
+def multipop(stack,n,cost):
+    for i in range(n):
+        m = int(input('Enter the number of elements to multipop : '))
+        for i in range(min(m,len(stack))):
+            stack.pop()
+            cost += 1
+    return stack,cost
+
+
+stack = []
+cost = 0
+op = 0
+n = int(input('Enter the number of elements to push (Cost/push == 1) : '))
+op += n
+stack,cost = push(stack,n,cost)
+print(f'Stack after {n} push operations : ',stack)
+print(f'Cost after {n} push operations : ',cost)
+
+n = int(input('Enter the number of Multipop opeartions : '))
+op += n
+stack,cost = multipop(stack,n,cost)
+print(f'Stack after {n} multipops : ',stack)
+print(f'Cost after {n} multipops : ',cost)
+
+amortized_cost = cost/op
+print('Amortized Cost per operation : ',amortized_cost)    
--- IS/playfair.py
+++ IS/playfair.py
@@ -1,40 +1,40 @@
 import numpy as np
-matrix = np.zeros((5,5))
-print(matrix)
-plain_text = 'COMMUNICATE'
-
-# unique_char = ''.join(set(plain_text))
-# print(unique_char)
 # Splitting the plaintext into digraphs
-diagraphs = []
-i = 0
-while i < len(plain_text):
-    diagraph = []
-    if i == len(plain_text) - 1:  
-        diagraph.append(plain_text[i])
-        diagraph.append('X')
-        i += 1
-    elif plain_text[i] != plain_text[i+1]:  
-        diagraph.append(plain_text[i])
-        diagraph.append(plain_text[i+1])
-        i += 2
-    else:  
-        diagraph.append(plain_text[i])
-        diagraph.append('X')
-        i += 1
-    diagraphs.append(diagraph)
+def get_diagraphs(plain_text):
+    diagraphs = []
+    i = 0
+    while i < len(plain_text):
+        diagraph = []
+        if i == len(plain_text) - 1:  
+            diagraph.append(plain_text[i])
+            diagraph.append('X')
+            i += 1
+        elif plain_text[i] != plain_text[i+1]:  
+            diagraph.append(plain_text[i])
+            diagraph.append(plain_text[i+1])
+            i += 2
+        else:  
+            diagraph.append(plain_text[i])
+            diagraph.append('X')
+            i += 1
+        diagraphs.append(diagraph)
+    return diagraphs
 
-print(diagraphs)
+
+def playfair_matrix(key):
+    matrix = [['' for _ in range(5)] for _ in range(5)]
+    alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'  # Exclude 'J'
+    key_index = 0
+
+    # Fill the matrix with unique characters from the key
+    
+    return matrix
+    
 
 
-def create_playfair_matrix(key):
-    key = key.replace(" ", "").upper()
-    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"  
-    key_set = set(key)
-    remaining_chars = [char for char in alphabet if char not in key_set]
-    key += ''.join(remaining_chars[:25 - len(key)])
-    return np.array([list(key[i:i+5]) for i in range(0, 25, 5)])
-
-keyword = 'ARADHYA'
-matrix = create_playfair_matrix(key=keyword)
+plain_text = 'COMMUNICATE'
+key = 'MONARCHY'
+diagraphs = get_diagraphs(plain_text)
+matrix = playfair_matrix(key)
 print(matrix)
+print(diagraphs)

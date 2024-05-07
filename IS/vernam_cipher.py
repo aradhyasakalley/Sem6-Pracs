56a27a23b41ff9daae3ca195ede5720492cf0f8c
aradhyasakalley <sakalleyaradhya@gmail.com>
2024-05-07 16:21:57 +0530

    vernam alt

--- IS/vernam_cipher.py
+++ IS/vernam_cipher.py
@@ -12,32 +12,26 @@ def key_padding(plain_text,key):
             i += 1
     return key
 
-def vernam_cipher(plain_text,key):
+def vernam_cipher(plain_text, key):
     ciphered_text = ''
-    for char1,char2 in zip(plain_text,key):
-       mapping_value1 = mapping[char1]
-       mapping_value2 = mapping[char2]
-       xor_value = (mapping_value1 ^ mapping_value2)
-       if xor_value > 26:
-           xor_value -= 26
-       
-       ciphered_text += chr(xor_value + 65)
+    for char1, char2 in zip(plain_text, key):
+        mapping_value1 = mapping[char1.upper()]
+        mapping_value2 = mapping[char2.upper()]
+        encrypted_value = (mapping_value1 + mapping_value2) % 26  
+        ciphered_text += chr(encrypted_value + 65)
     return ciphered_text 
 
-def vernam_decipher(plain_text,key):
-    ciphered_text = ''
-    for char1,char2 in zip(plain_text,key):
-       mapping_value1 = mapping[char1]
-       mapping_value2 = mapping[char2]
-       xor_value = (mapping_value1 ^ mapping_value2)
-       if xor_value > 26:
-           xor_value -= 26
-       
-       ciphered_text += chr(xor_value + 65)
-    return ciphered_text 
+def vernam_decipher(ciphered_text, key):
+    deciphered_text = ''
+    for char1, char2 in zip(ciphered_text, key):
+        mapping_value1 = mapping[char1.upper()]
+        mapping_value2 = mapping[char2.upper()]
+        decrypted_value = (mapping_value1 - mapping_value2) % 26  
+        deciphered_text += chr(decrypted_value + 65)
+    return deciphered_text 
 
 
-print(2^18)
+
 plain_text = 'OAK'
 plain_text = plain_text.replace(' ','')
 print(plain_text)

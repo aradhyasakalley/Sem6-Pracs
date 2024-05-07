a4e3d5654e3fd07a28f03862ca572cb45d82735a
aradhyasakalley <sakalleyaradhya@gmail.com>
2024-05-07 20:09:52 +0530

    Ford fulkerson

--- AA/ford-fulkerson.py
+++ AA/ford-fulkerson.py
@@ -1,12 +1,17 @@
 
 def ford_fulkerson(possible_paths,graph):
     flow = 0
+    # Traversing each possible path one by one
     for path in possible_paths:
         min_edge = float('inf')
+        # Traversing each edge of the path
+        # Finding the augmenting (minimum edge)
         for i in range(len(path)-1):
             edge_length = graph[path[i]][path[i+1]]
             min_edge = min(min_edge,edge_length)
+        # Updating the capacities and handling bottlenecks (full flow)
         for i in range(len(path)-1):
+            # When edge in full flow
             if graph[path[i]][path[i+1]] == 0:
                 print(f'Path {path} not possible')
                 break
@@ -15,6 +20,7 @@ def ford_fulkerson(possible_paths,graph):
         flow += min_edge
     print(flow)
 
+# DFS approach to find all possible paths
 def dfs_paths(graph, start, goal, path=[]):
     path = path + [start]
     if start == goal:
@@ -35,17 +41,22 @@ graph = {
     'B': {'T': 3}
 }
 
-
-graph2 = {
-    'S' : {'A' : 100, 'B' : 100},
-    'A': {'C': 7},
-    'B': {'C': 3},
-    'C':{'T':1}
-}
+# graph2 = {
+#     'S' : {'A' : 100, 'B' : 100},
+#     'A': {'C': 7},
+#     'B': {'C': 3},
+#     'C':{'T':1}
+# }
+# graph3 = {
+#     'S' : {'A' : 9, 'D' : 3},
+#     'A': {'B': 9},
+#     'B': {'T': 2},
+#     'D': {'C':4,'B':7},
+#     'C':{'T':5}
+# }
 
 source = 'S'
 target = 'T'
-
-possible_paths = dfs_paths(graph2, source, target)
+possible_paths = dfs_paths(graph, source, target)
 print("All possible paths from", source, "to", target, ":", possible_paths)
-ford_fulkerson(possible_paths,graph2)
+ford_fulkerson(possible_paths,graph)

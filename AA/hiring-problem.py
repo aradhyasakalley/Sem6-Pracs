f627c78beec1941606994eba242831d5607fb427
aradhyasakalley <sakalleyaradhya@gmail.com>
2024-05-08 11:56:23 +0530

    hiring problem fix

--- AA/hiring-problem.py
+++ AA/hiring-problem.py
@@ -6,7 +6,7 @@ def hiring_problem(candidates):
     interview_cost = 5
     pay_per_day  = 2
     current_best = 0
-    days_worked = 1
+    days_worked = 0
     candidates_interviewed = 0
     cost = 0  
     for candidate in candidates:
@@ -24,15 +24,14 @@ def hiring_problem(candidates):
 
 
 # Worst Case
-# candidates = [1,2,3,4,5,6,7,8,9,10]
+candidates = [1,2,3,4,5,6,7,8,9,10]
 # Best Case
 # candidates = [10,9,8,7,6,5,4,3,2,1]
 # Average Case
-candidates = random.sample(range(1, 11), 10)
+# candidates = random.sample(range(1, 11), 10)
 print(candidates)
 cost, candidates_interviewed = hiring_problem(candidates)
 print("Total cost:", cost)
 print("Candidates interviewed:", candidates_interviewed)
-# The formula cost should be equal to or greater than equal to total cost ????
 formula_cost = 10*5 + candidates_interviewed*10
 print(formula_cost)
